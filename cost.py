1973169e358410ba173da36532fa233a78d4801d
Adi-Levy <adilevy15@gmail.com>
2021-03-23 20:54:01 +0200

    minor naming changes and additions

--- cost.py
+++ cost.py
@@ -20,26 +20,29 @@ def L(state, command):
 def C(slack):
     return q_s * slack + q_ss * (slack**2)
 
-def step(state, command, path):
+def do_step(state, command, path):
     integrator.model.State = 
     return integrator.RK4(command[1],command[0])
 
 def step_cost(state, command, path, slack):
     return J(state, path) + R(command) + L(state, command) + C(slack)
 
-    """
-        params:
-        -------------
-        state - np.array, vehicle state after blend
-        commands - 2 x horizon np.array, commands for cost calc
-        slack - 1 x horizon np.array, slack vars to be optimized
 
-        return:
-        --------------
-        total_cost - cost of given command matrix
-        horizon_state - last state to be in after horizon drive
-        steps_cost - cost of every step along the way, 1 x horizon vector
-    """
+"""
+    params:
+    -------------
+    state - np.array, vehicle state after blend
+    commands - 2 x horizon np.array, commands for cost calc
+                commands[i][0] - D
+                commands[i][1] - delta
+    slack - 1 x horizon np.array, slack vars to be optimized
+    
+    return:
+    --------------
+    total_cost - cost of given command matrix
+    horizon_state - last state to be in after horizon drive
+    steps_cost - cost of every step along the way, 1 x horizon vector
+"""
 def total_cost(state, commands, slack, horizon_state = None, steps_cost = None, prev_total_cost = 0):
     total_cost = 0
     
@@ -51,12 +54,12 @@ def total_cost(state, commands, slack, horizon_state = None, steps_cost = None, 
             step_c = step_cost(state,commands[step],path,slack[step])
             steps_cost.put(step_c)
             total_cost += step_c
-            state = step(state,commands[step])
+            state = do_step(state,commands[step])
     else:
         step_c = step_cost(state,commands[0],path,slack[0])
         total_cost = prev_total_cost - steps_cost.get(0) + step_c
         steps_cost.put(step_c)
-        state = step(horizon_state,commands[0])
+        state = do_step(horizon_state,commands[0])
         
     return total_cost, state, steps_cost
     
