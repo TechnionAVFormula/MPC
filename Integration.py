720e3ed1db51ee7f8b856e146bc662afb6daa517
Adi-Levy <adilevy15@gmail.com>
2021-03-23 19:53:48 +0200

    blend before integration, cost calc WIP

--- Integration.py
+++ Integration.py
@@ -1,3 +1,6 @@
+import math 
+import numpy as np
+
 class Integration:
     def __init__(self, model, dt=0.1):
         """
@@ -29,13 +32,29 @@ class Integration:
         D     - Driving command 
         """
 
-        k1 = self.model.state_derivative(self.model.State, delta, D)
-        k2 = self.model.state_derivative(self.model.State + self.dt * k1 / 2, delta, D)
-        k3 = self.model.state_derivative(self.model.State + self.dt * k2 / 2, delta, D)
-        k4 = self.model.state_derivative(self.model.State + self.dt * k3, delta, D)
+        k1 = self.blend(self.State, delta, D)
+        k2 = self.blend(self.State + self.dt * k1 / 2, delta, D)
+        k3 = self.blend(self.State + self.dt * k2 / 2, delta, D)
+        k4 = self.blend(self.State + self.dt * k3, delta, D)
 
-        self.model.State = self.model.State + 1 / 6 * self.dt * (k1 + 2 * k2 + 2 * k3 + k4)
+        self.State = self.State + 1 / 6 * self.dt * (k1 + 2 * k2 + 2 * k3 + k4)
 
+        # k1 = self.model.state_derivative(self.model.State, delta, D)
+        # k2 = self.model.state_derivative(self.model.State + self.dt * k1 / 2, delta, D)
+        # k3 = self.model.state_derivative(self.model.State + self.dt * k2 / 2, delta, D)
+        # k4 = self.model.state_derivative(self.model.State + self.dt * k3, delta, D)
+
+        # self.model.State = self.model.State + 1 / 6 * self.dt * (k1 + 2 * k2 + 2 * k3 + k4)
+   
+    def lambda(self, Vx):
+        Vx_blend_max = 5
+        Vx_blend_min = 3
+        return min(max((Vx-Vx_blend_min)/(Vx_blend_max-Vx_blend_min),0),1)
+
+    def blend(self, state, delta, D):
+        dyn_state_deriv = self.DynamicState.state_derivative(state, delta, D)
+        kin_state_deriv = self.KinModel.state_derivative(state, delta, D)
+        return np.array(lambda(dyn_state_deriv[0]) * dyn_state_deriv) + np.array((1 - lambda(kin_state_deriv[0])) * kin_state_deriv)
 
 def main():
     pass
--- cost.py
+++ cost.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from queue import Queue
 
 cost_R = np.array([[R_Drive, 0], [0, R_delta]])
 
@@ -16,22 +17,46 @@ def L(state, command):
     beta_kin = math.atan2(math.tan(command[1]) * L_REAR / (L_REAR + L_FRONT))
     return  q_beta * (beta_kin - beta_dyn)**2 
 
-def C():
-    return 
+def C(slack):
+    return q_s * slack + q_ss * (slack**2)
 
 def step(state, command, path):
-    return 
+    integrator.model.State = 
+    return integrator.RK4(command[1],command[0])
 
-def step_cost(state, command, path):
-    return J(state, path) + R(command) + L(state, command) + C()
+def step_cost(state, command, path, slack):
+    return J(state, path) + R(command) + L(state, command) + C(slack)
 
-def total_cost():
+    """
+        params:
+        -------------
+        state - np.array, vehicle state after blend
+        commands - 2 x horizon np.array, commands for cost calc
+        slack - 1 x horizon np.array, slack vars to be optimized
+
+        return:
+        --------------
+        total_cost - cost of given command matrix
+        horizon_state - last state to be in after horizon drive
+        steps_cost - cost of every step along the way, 1 x horizon vector
+    """
+def total_cost(state, commands, slack, horizon_state = None, steps_cost = None, prev_total_cost = 0):
     total_cost = 0
-    for step in range(horizon):
-        if not check_constraints(state,commands[step]):
-            return math.inf
-        total_cost += step_cost(state,commands[step],path)
-        state = step(state,commands[step])
     
-    return total_cost
+    if len(commands) > 1:
+        steps_cost = Queue()
+        for step in range(horizon):
+            if not check_constraints(state,commands[step],slack[step]):
+                return math.inf, None, None
+            step_c = step_cost(state,commands[step],path,slack[step])
+            steps_cost.put(step_c)
+            total_cost += step_c
+            state = step(state,commands[step])
+    else:
+        step_c = step_cost(state,commands[0],path,slack[0])
+        total_cost = prev_total_cost - steps_cost.get(0) + step_c
+        steps_cost.put(step_c)
+        state = step(horizon_state,commands[0])
+        
+    return total_cost, state, steps_cost
     
