a7b44c9000b5e91acf9b16a0fb83b1496c73b787
SharonGoldi <50270028+SharonGoldi@users.noreply.github.com>
2021-06-15 15:08:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,27 +11,27 @@ def normal_dist_vec(vec, mean=0, std=0.5):
 def main():
     # test the DMD
     params = []
-    min_func = gd_loss()
-    step_func = gd_step()
-    control_dist = normal_dist_vec()
+    min_func = gd_loss
+    step_func = gd_step
+    control_dist = normal_dist_vec
     state_uncertainty = 0.5
     sub_opt_depth = 10
     path_str_center = [0, 0, 1, 0]
     path_str_side = [0, 0, 1, 0.5]
     path_par_center = [0, 1, 0, 0]
-    dmd = DMD(params, min_func, step_func, control_dist, state_uncertainty, sub_opt_depth, path_str_center, learn_rate=1e-3)
-    vec = dmd.step()
-    
+    dmd = DMD(params, min_func, step_func, control_dist, state_uncertainty, sub_opt_depth, path_str_center,
+              learn_rate=1e-3)
+    commands = dmd.step()
 
-    # # test - drive straight as fast as you can
-    # right_edge = [0, 0, 0, 3]
-    # left_edge = [0, 0, 0, -3]
+    # test - drive straight as fast as you can, check with sim
+    right_edge = [0, 0, 0, 3]
+    left_edge = [0, 0, 0, -3]
     # commands = []
     # for i in range(300):
-    #     commands.append([0.3, 0.01])
-    #
-    # sim = Simulator(right_edge=right_edge, left_edge=left_edge, commands=commands)
-    # sim.plot_final_drive()
+    #     commands.append([0., 0.01])
+
+    sim = Simulator(right_edge=right_edge, left_edge=left_edge, commands=commands)
+    sim.plot_final_drive()
 
 
 if __name__ == "__main__":
